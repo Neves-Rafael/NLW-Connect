9c465af90898fc6e9da8281f5f0cd0a6aa41d9fc
Rafael Neves <nevesrafael.dev@gmail.com>
2025-02-19 21:18:49 -0300

    :sparkles: Feat get-subscriber-invites-count route and function

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -1,13 +1,15 @@
 import { eq } from "drizzle-orm";
 import { db } from "../drizzle/client";
 import { subscriptions } from "../drizzle/schema/subscriptions";
+import { redis } from "../redis/client";
 
 interface subscribeToEventParams {
   name: string;
   email: string;
+  referrerId?: string | null;
 }
 
-export async function subscribeToEvent({ name, email }: subscribeToEventParams) {
+export async function subscribeToEvent({ name, email, referrerId }: subscribeToEventParams) {
   const subscribers = await db.select().from(subscriptions).where(eq(subscriptions.email, email));
 
   if (subscribers.length > 0) {
@@ -21,6 +23,10 @@ export async function subscribeToEvent({ name, email }: subscribeToEventParams) 
       email,
     })
     .returning();
+
+  if (referrerId) {
+    await redis.zincrby("referral:ranking", 1, referrerId);
+  }
 
   const subscriber = result[0];
 
